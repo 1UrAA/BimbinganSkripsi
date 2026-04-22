67c2108d1c7b5294dd6654ebde59758f669f9a8d
Pongo 725 <pongo725@example.com>
2026-04-22 13:56:41 +0800

    modified:   BimbinganSkripsi/.env.example 	modified:   BimbinganSkripsi/app/Http/Controllers/AkunAdminController.php

--- BimbinganSkripsi/app/Http/Controllers/AkunAdminController.php
+++ BimbinganSkripsi/app/Http/Controllers/AkunAdminController.php
@@ -10,35 +10,42 @@ use Illuminate\Support\Facades\Auth;
 
 class AkunAdminController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $data = User::where('role', 'admin_prodi')->with('prodi')->get();
         return view('admin_prodi.index', compact('data'));
     }
 
-    public function create() {
+    public function create()
+    {
         $prodis = Prodi::all();
         return view('admin_prodi.create', compact('prodis'));
     }
 
-    public function store(Request $request) {
-         $request->validate([
-             'name' => 'required',
-             'email' => 'required|email|unique:users',
-             'password' => 'required|min:6',
-             'prodi_id' => 'required'
-         ]);
-         User::create([
-             'name' => $request->name,
-             'email' => $request->email,
-             'password' => Hash::make($request->password),
-             'role' => 'admin_prodi',
-             'prodi_id' => $request->prodi_id
-         ]);
-         return redirect()->route('akun_admin.index');
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'prodi_id' => 'required'
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'admin_prodi',
+            'prodi_id' => $request->prodi_id
+        ]);
+        return redirect()->route('akun_admin.index');
     }
 
-    public function destroy($id) {
-         User::destroy($id);
-         return back();
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return back();
     }
 }
+
+
+//eja jomok
